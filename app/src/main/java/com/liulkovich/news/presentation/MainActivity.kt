6157921826cee16db4bb0e3b1abd9d4e569bbf17
Ivan Liulkovich <ahapxuct09@gmail.com>
2025-10-10 11:57:59 +0300

    Notifications

--- app/src/main/java/com/liulkovich/news/presentation/MainActivity.kt
+++ app/src/main/java/com/liulkovich/news/presentation/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.liulkovich.news.presentation
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import com.liulkovich.news.presentation.screen.subscriptions.SubscriptionsScreen
 import com.liulkovich.news.presentation.ui.theme.NewsTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.contracts.contract
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -16,9 +20,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NewsTheme {
+                val permissionLauncher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.RequestPermission(),
+                    onResult = {}
+                )
                 SubscriptionsScreen(
                     onNavigateToSettings = {
-
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                            permissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+                        }
                     }
                 )
             }
