15fe225963947d91b46cb5aeb066353341f59afb
Ivan Liulkovich <ahapxuct09@gmail.com>
2025-10-13 12:00:38 +0300

    Pending Intent

--- app/src/main/java/com/liulkovich/news/data/background/NotificationsHelper.kt
+++ app/src/main/java/com/liulkovich/news/data/background/NotificationsHelper.kt
@@ -2,11 +2,14 @@ package com.liulkovich.news.data.background
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.content.getSystemService
 import com.liulkovich.news.R
+import com.liulkovich.news.presentation.MainActivity
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
@@ -32,9 +35,22 @@ class NotificationsHelper @Inject constructor(
     }
 
     fun showNewArticlesNotification(topics: List<String>) {
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+        }
+
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            PENDING_INTENT_RC,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
         val notification = NotificationCompat.Builder(context, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_breaking_news)
             .setContentTitle(context.getString(R.string.new_articles_notifcation_title))
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
             .setContentText(
                 context.getString(
                     R.string.update_subscriptions,
@@ -49,5 +65,6 @@ class NotificationsHelper @Inject constructor(
     companion object {
         private const val CHANNEL_ID = "new_articles"
         private const val NOTIFICATION_ID = 1
+        private const val PENDING_INTENT_RC = 1
     }
 }
