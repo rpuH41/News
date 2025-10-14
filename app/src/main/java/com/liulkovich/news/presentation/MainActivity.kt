52cbbe4792598b2a0ff0ac06740c79c560c65294
Ivan Liulkovich <ahapxuct09@gmail.com>
2025-10-14 19:40:07 +0300

    NavGraph

--- app/src/main/java/com/liulkovich/news/presentation/MainActivity.kt
+++ app/src/main/java/com/liulkovich/news/presentation/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.liulkovich.news.presentation.screen.settings.SettingsScreen
+import com.liulkovich.news.presentation.navigation.NavGraph
 import com.liulkovich.news.presentation.ui.theme.NewsTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,10 +16,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NewsTheme {
-
-                SettingsScreen(
-                    onBackClick = {}
-                )
+                NavGraph()
             }
         }
     }
